117f6b80b69a6e8e1345d4bed014b167ee162aa2
Murugesh <vmurugesh168@gmail.com>
2025-10-17 11:06:32 +0530

    Add files via upload
    added files

---
+++ object_funcmethod.js
@@ -0,0 +1,29 @@
+// var new_console = {
+//     newlog : function (){
+//         console.log ("HELLO");
+//     },
+// }
+// new_console.newlog();
+
+// var sample = function greet(){
+//      console.log("WELCOME");
+//  }
+// sample();
+
+// var sample = function greet(){
+//     console.log("WELCOME");
+// }
+// var result = sample();
+// console.log(result);
+ 
+var new_console = {
+    newOne :"HELLO",
+    newTwo :"HI THERE",
+    newThree : function(){
+        return "Long Time No See";
+    }
+}
+console.log(new_console.newTwo);
+console.log(new_console.newThree());
+
+
---
+++ object_methods.js
@@ -0,0 +1,19 @@
+var student ={
+    name : "Ajith",
+    age : 21,
+    college : "SSIT",
+    designation : "Mechanical Engineer"
+}
+console.log(Object.keys(student));
+console.log(Object.values(student));
+console.log(Object.entries(student));
+
+var employee ={
+    name : "MURUGESH V",
+    age : 21,
+    designation : "MERN FULLSTACK DEVELOPER"
+}
+console.log(Object.keys(employee));
+console.log(Object.values(employee));
+console.log(Object.entries(employee));
+
