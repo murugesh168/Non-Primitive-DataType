4b7b1b4d4f6cc47af237fe736c61add9b78a2a26
Murugesh <vmurugesh168@gmail.com>
2025-10-17 11:07:39 +0530

    Update object_funcmethod.js

--- object_funcmethod.js
+++ object_funcmethod.js
@@ -1,21 +1,25 @@
-// var new_console = {
-//     newlog : function (){
-//         console.log ("HELLO");
-//     },
-// }
-// new_console.newlog();
+//
+var new_console = {
+    newlog : function (){
+        console.log ("HELLO");
+    },
+}
+new_console.newlog();
 
-// var sample = function greet(){
-//      console.log("WELCOME");
-//  }
-// sample();
+//
+var sample = function greet(){
+     console.log("WELCOME");
+ }
+sample();
 
-// var sample = function greet(){
-//     console.log("WELCOME");
-// }
-// var result = sample();
-// console.log(result);
- 
+//
+var sample = function greet(){
+    console.log("WELCOME");
+}
+var result = sample();
+console.log(result);
+
+//
 var new_console = {
     newOne :"HELLO",
     newTwo :"HI THERE",
@@ -25,5 +29,6 @@ var new_console = {
 }
 console.log(new_console.newTwo);
 console.log(new_console.newThree());
+
 
 
